e3b4f73eebac6004e5b3d0d3bb23881648882827
jodyneckles <jody@technative.tv>
2019-03-04 15:55:09 +0000

    enhanced nugget serializer to include theme and workspace information

--- app/serializers/nugget_serializer.rb
+++ app/serializers/nugget_serializer.rb
@@ -1,9 +1,14 @@
 class NuggetSerializer < ActiveModel::Serializer
-  attributes :id, :title, :img_url, :video_url, :question, :body
-  belongs_to :theme
+  attributes :id, :title, :img_url, :video_url, :question, :theme
+  # belongs_to :theme
   has_many :nugget_tags
   has_many :user_nuggets
-  has_many :users
+  belongs_to :theme
+
+  class ThemeSerializer < ActiveModel::Serializer
+    attributes :name, :workspace
+  end
+
   # class NuggetTagSerializer < ActiveModel::Serializer
   #   attributes :id, :nugget_id, :tag_id
   # end
